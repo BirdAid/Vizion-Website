90d88641fae0001b8ec1ed308ebf873e6f3e6913
BirdAid <stherasquad@gmail.com>
2021-08-18 04:43:42 +0200

    made final Changes

--- js/myscript.js
+++ js/myscript.js
@@ -13,5 +13,26 @@ contactForm.addEventListener('submit', (e)=>{
         subject: subject.value,
         message: message.value
     }
-    console.lof('formData');
+    console.log('formData');
 })
+
+// countdown
+
+var countDownDate = new Date("Jan 1, 2022 15:30:30").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    
+    var days = Math.floor(distance/(1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="countdown"
+    document.getElementById("countdown").innerHTML = days + "d " + hours +"h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "ITS HERE";
+    }
+});
